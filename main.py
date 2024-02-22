15b07ea7f7c0dea6d1d74b16b7e6274c5aaf2905
kuhhcm <158571357+kuhhcm@users.noreply.github.com>
2024-02-23 01:21:56 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,71 +1,245 @@
-class Car:
-    def __init__(self):
-        self.model = ""
-        self.year = 0
-        self.manufacturer = ""
-        self.engine_volume = 0.0
-        self.color = ""
-        self.price = 0.0
+# class Car:
+#     def __init__(self):
+#         self.model = ""
+#         self.year = 0
+#         self.manufacturer = ""
+#         self.engine_volume = 0.0
+#         self.color = ""
+#         self.price = 0.0
 
-    def input_data(self):
-        self.model = input("Enter the model name: ")
-        self.year = int(input("Enter the year of manufacture: "))
-        self.manufacturer = input("Enter the manufacturer: ")
-        self.engine_volume = float(input("Enter the engine volume: "))
-        self.color = input("Enter the car color: ")
-        self.price = float(input("Enter the price: "))
+#     def input_data(self):
+#         self.model = input("Enter the model name: ")
+#         self.year = int(input("Enter the year of manufacture: "))
+#         self.manufacturer = input("Enter the manufacturer: ")
+#         self.engine_volume = float(input("Enter the engine volume: "))
+#         self.color = input("Enter the car color: ")
+#         self.price = float(input("Enter the price: "))
 
-    def output_data(self):
-        print("Model:", self.model)
-        print("Year of manufacture:", self.year)
-        print("Manufacturer:", self.manufacturer)
-        print("Engine volume:", self.engine_volume)
-        print("Car color:", self.color)
-        print("Price:", self.price)
+#     def output_data(self):
+#         print("Model:", self.model)
+#         print("Year of manufacture:", self.year)
+#         print("Manufacturer:", self.manufacturer)
+#         print("Engine volume:", self.engine_volume)
+#         print("Car color:", self.color)
+#         print("Price:", self.price)
 
-    def get_model(self):
-        return self.model
+#     def get_model(self):
+#         return self.model
 
-    def get_year(self):
-        return self.year
+#     def get_year(self):
+#         return self.year
 
-    def get_manufacturer(self):
-        return self.manufacturer
+#     def get_manufacturer(self):
+#         return self.manufacturer
 
-    def get_engine_volume(self):
-        return self.engine_volume
+#     def get_engine_volume(self):
+#         return self.engine_volume
 
-    def get_color(self):
-        return self.color
+#     def get_color(self):
+#         return self.color
 
-    def get_price(self):
-        return self.price
+#     def get_price(self):
+#         return self.price
 
-    def set_model(self, model):
-        self.model = model
+#     def set_model(self, model):
+#         self.model = model
 
-    def set_year(self, year):
-        self.year = year
+#     def set_year(self, year):
+#         self.year = year
 
-    def set_manufacturer(self, manufacturer):
-        self.manufacturer = manufacturer
+#     def set_manufacturer(self, manufacturer):
+#         self.manufacturer = manufacturer
 
-    def set_engine_volume(self, engine_volume):
-        self.engine_volume = engine_volume
+#     def set_engine_volume(self, engine_volume):
+#         self.engine_volume = engine_volume
 
-    def set_color(self, color):
-        self.color = color
+#     def set_color(self, color):
+#         self.color = color
 
-    def set_price(self, price):
-        self.price = price
+#     def set_price(self, price):
+#         self.price = price
 
 
-if __name__ == "__main__":
-    car1 = Car()
-    car1.input_data()
+# if __name__ == "__main__":
+#     car1 = Car()
+#     car1.input_data()
 
-    print("Car data:")
-    car1.output_data()
+#     print("Car data:")
+#     car1.output_data()
 
-    print("Year of manufacture:", car1.get_year())
-    print("Car color:", car1.get_color())
+#     print("Year of manufacture:", car1.get_year())
+#     print("Car color:", car1.get_color())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Book:
+#     def __init__(self):
+#         self.title = ""
+#         self.year = 0
+#         self.publisher = ""
+#         self.genre = ""
+#         self.author = ""
+#         self.price = 0.0
+
+#     def input_data(self):
+#         self.title = input("Enter the book title: ")
+#         self.year = int(input("Enter the publication year: "))
+#         self.publisher = input("Enter the publisher: ")
+#         self.genre = input("Enter the genre: ")
+#         self.author = input("Enter the author: ")
+#         self.price = float(input("Enter the price: "))
+
+#     def output_data(self):
+#         print("Title:", self.title)
+#         print("Publication Year:", self.year)
+#         print("Publisher:", self.publisher)
+#         print("Genre:", self.genre)
+#         print("Author:", self.author)
+#         print("Price:", self.price)
+
+#     def get_title(self):
+#         return self.title
+
+#     def get_year(self):
+#         return self.year
+
+#     def get_publisher(self):
+#         return self.publisher
+
+#     def get_genre(self):
+#         return self.genre
+
+#     def get_author(self):
+#         return self.author
+
+#     def get_price(self):
+#         return self.price
+
+#     def set_title(self, title):
+#         self.title = title
+
+#     def set_year(self, year):
+#         self.year = year
+
+#     def set_publisher(self, publisher):
+#         self.publisher = publisher
+
+#     def set_genre(self, genre):
+#         self.genre = genre
+
+#     def set_author(self, author):
+#         self.author = author
+
+#     def set_price(self, price):
+#         self.price = price
+
+
+# if __name__ == "__main__":
+#     book1 = Book()
+#     book1.input_data()
+
+#     print("Book data:")
+#     book1.output_data()
+
+#     print("Publication Year:", book1.get_year())
+#     print("Author:", book1.get_author())
+
+#     book1.set_price(19.99)
+    # print("Modified price:", book1.get_price())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Stadium:
+#     def __init__(self):
+#         self.name = ""
+#         self.opening_date = ""
+#         self.country = ""
+#         self.city = ""
+#         self.capacity = 0
+
+#     def input_data(self):
+#         self.name = input("Enter the stadium name: ")
+#         self.opening_date = input("Enter the opening date: ")
+#         self.country = input("Enter the country: ")
+#         self.city = input("Enter the city: ")
+#         self.capacity = int(input("Enter the seating capacity: "))
+
+#     def output_data(self):
+#         print("Stadium Name:", self.name)
+#         print("Opening Date:", self.opening_date)
+#         print("Country:", self.country)
+#         print("City:", self.city)
+#         print("Seating Capacity:", self.capacity)
+
+#     def get_name(self):
+#         return self.name
+
+#     def get_opening_date(self):
+#         return self.opening_date
+
+#     def get_country(self):
+#         return self.country
+
+#     def get_city(self):
+#         return self.city
+
+#     def get_capacity(self):
+#         return self.capacity
+
+#     def set_name(self, name):
+#         self.name = name
+
+#     def set_opening_date(self, opening_date):
+#         self.opening_date = opening_date
+
+#     def set_country(self, country):
+#         self.country = country
+
+#     def set_city(self, city):
+#         self.city = city
+
+#     def set_capacity(self, capacity):
+#         self.capacity = capacity
+
+
+# if __name__ == "__main__":
+#     stadium1 = Stadium()
+#     stadium1.input_data()
+
+#     print("Stadium data:")
+#     stadium1.output_data()
+
+#     print("Country:", stadium1.get_country())
+#     print("Seating Capacity:", stadium1.get_capacity())
+
+#     stadium1.set_name("New Stadium")
+#     print("Modified Stadium Name:", stadium1.get_name())
+
+
+
